b93562a1cca9b11e3960d0f4f3ac71595c2ba885
Benjamin Patton <bass41992ben@gmail.com>
2025-01-25 10:05:31 -0500

    added more markdown helpers and changed to netlify

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,6 +4,8 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
+const postsDirectory = path.join(process.cwd(), "app/content/posts");
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -60,3 +62,82 @@ export interface Post {
   updated?: string;
   categories: string[];
 }
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const { data, content } = matter(fileContents);
+
+  //Use remark to convert markdown into Html string
+  const processedContent = {
+    content,
+    data,
+  };
+
+  // Combine the data with the id
+  return {
+    id,
+    data,
+    content,
+  };
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data } = matter(fileContents);
+    
+    return {
+      id,
+      title: data.title,
+      slug: data.slug,
+      generatedSlug: id,
+      excerpt: data.excerpt,
+      coverImage: data.coverImage,
+      coverWidth: data.coverWidth,
+      coverHeight: data.coverHeight,
+      published: data.published,
+      categories: data.categories || [],
+    } as Post;
+  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.published < b.published) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
